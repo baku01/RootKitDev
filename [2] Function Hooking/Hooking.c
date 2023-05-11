bc80fc9507454e92e2e15ff55c78a2b3d07b2dad
L0gic <95774215+baku01@users.noreply.github.com>
2023-05-11 11:57:26 -0300

    modified:   [2] Function Hooking/Hooking.c

--- [2] Function Hooking/Hooking.c
+++ [2] Function Hooking/Hooking.c
@@ -10,16 +10,16 @@ MODULE_AUTHOR("l0gic");
 MODULE_DESCRIPTION("mkdir syscall hook");
 MODULE_VERSION("0.01");
 
-#if defined(CONFIG_X86_64) && (LINUX_VERSION_CODE >= KERNEL_VERSION(5,19,0))
+#if defined(CONFIG_X86_64) && (LINUX_VERSION_CODE >= KERNEL_VERSION(4,17,0))
 #define PTREGS_SYSCALL_STUBS 1
 #endif
 
 #ifdef PTREGS_SYSCALL_STUBS
-static asmlinkage long (*org_mkdir)(const struct pt_regs *)
+static asmlinkage long (*org_mkdir)(const struct pt_regs *);
 
 asmlinkage int Hook_no_Mkdir(const struct pt_regs *regs)
 {
-    char __user *pathname = (char *) regs->di;
+    char __user *pathname = (char *)regs->di;
     char dir_name[NAME_MAX] = {0};
 
     long error = strncpy_from_user(dir_name, pathname, NAME_MAX);
@@ -30,9 +30,9 @@ asmlinkage int Hook_no_Mkdir(const struct pt_regs *regs)
         return 0; 
 }
 #else
-static asmlinkage long (*orig_mkdir)(const char __user *pathname, umode_t mode);
+static asmlinkage long (*org_mkdir)(const char __user *pathname, umode_t mode);
 
-asmlinkage int Hook_no_Mkdir(const char __user *pathname, unomode_t mode)
+asmlinkage int Hook_no_Mkdir(const char __user *pathname, unmode_t mode)
 {
     char dir_name[NAME_MAX] = {0};
 
